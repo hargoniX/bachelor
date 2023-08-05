792106109ff23319fe00ed4fd4f51026dfa2e057
Henrik Boeving <boeving@hm.edu>
2023-08-06 00:24:00 +0200

    chore: update nix

--- flake.nix
+++ flake.nix
@@ -2,17 +2,15 @@
   description = "Bachelor Thesis";
 
   inputs.typst-lsp.url = "github:nvarner/typst-lsp";
-  inputs.typst.follows = "typst-lsp/typst";
+  inputs.typst.url = "github:typst/typst";
   inputs.utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
 
   outputs = {
     nixpkgs,
     utils,
     typst,
     typst-lsp,
-    pre-commit-hooks,
     ...
   }:
     utils.lib.eachDefaultSystem (system: let
@@ -23,16 +21,7 @@
         });
       };
       pkgs = nixpkgs.legacyPackages.${system}.appendOverlays [typst-overlay];
-      pre-commit-check = pre-commit-hooks.lib.${system}.run {
-        src = ./.;
-        hooks = {
-          alejandra.enable = true;
-          deadnix.enable = true;
-          statix.enable = true;
-        };
-      };
       typst-shell = pkgs.mkShell {
-        inherit (pre-commit-check) shellHook;
         nativeBuildInputs = [
           pkgs.typst-lsp
           pkgs.typst
@@ -42,6 +31,5 @@
       devShells.default = typst-shell;
       overlays.default = typst-overlay;
       legacyPackages = pkgs;
-      checks.formatting = pre-commit-check;
     });
 }
